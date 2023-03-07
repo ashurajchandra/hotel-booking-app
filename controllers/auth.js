fccb7b99ef2f0a6673c69cbb9ac3e3740e668a24
ashurajchandra <mukutrai12@gmail.com>
2023-03-08 01:02:48 +0530

    setup basic routing

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+
+
+module.exports.register = async(req,res)=>{
+    try{
+     return
+    }catch(error){
+        console.log("error occured while register user",error)
+    }
+};
+
+
+module.exports.login = async(req,res)=>{
+    try{
+      return
+    }catch(error){
+        console.log("error occured while register user",error)
+    }
+}
--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const routes = require("./routes");
+app.use("/",routes)
 app.listen(PORT, (req,res)=>{console.log("app is up and listening on port",PORT)})
